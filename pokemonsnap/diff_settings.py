2fcd822f376240d716c4fb5559ea49b79fc66e04
Ethan Roseman <ethteck@gmail.com>
2020-07-11 22:00:38 -0400

    Diff and .gitignore

---
+++ pokemonsnap/diff_settings.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+def apply(config, args):
+    config['baseimg'] = '../baserom.z64'
+    config['myimg'] = 'pokemonsnap.z64'
+    config['mapfile'] = 'build/pokemonsnap.map'
+    config['source_directories'] = ['.']

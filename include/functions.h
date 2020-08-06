946d063f4cd731cddf44d7ed581b4a464f488b26
Ethan Roseman <ethteck@gmail.com>
2020-08-05 20:52:22 -0400

    matched volcano_code_72F590

--- include/functions.h
+++ include/functions.h
@@ -6,6 +6,8 @@
 /* 0x80008C28 */ GObj* runGObjProcess(GObj*, gfxFunc* func, s8 kind, u32 priority);
 /* 0x80008F2C */ void endGObjProcess(GObj*);
 
+/* 0x8000B830 */ void sendSignalToLink(s32 llIndex, s32, GObj*);
+
 /* 0x8009B980 */ s32 getLevelId();
 /* 0x8009B98C */ void setLevelId(s32 levelID);
 /* 0x8009B998 */ s8* getLevelName(s32 levelIdx);
@@ -19,11 +21,17 @@
 
 /* 0x802D2580 */ void spawnStaryuAtGeo(GObj*);
 /* 0x802D25B0 */ void spawnStarmieAtGeo(GObj*);
+
 /* 0x8035EC58 */ void updateAnimalState(GObj* obj, gfxFunc* state);
 /* 0x8035ECAC */ void weightedRandomStaightTransition(GObj* obj, randomTransition* nextStates);
+/* 0x8035EDF8 */ void runPathProcess(GObj* obj, gfxFunc* func);
+/* 0x8035F138 */ void setAnimalAnimation(s32, animationHeader*);
+/* 0x8035F15C */ void forceAnimalAnimation(s32, animationHeader*);
 /* 0x8035FBF0 */ void runInteractionsAndWaitForFlags(GObj* obj, u32 flags);
 /* 0x8035FD70 */ void runAnimalCleanup(GObj* obj);
 /* 0x8035FE24 */ GObj* addAnimalAtGeo(GObj* obj, u32 id, animalDef* def);
+/* 0x80362050 */ roomGFX* setNodePosToNegRoom(GObj*);
+/* 0x803620C8 */ void animalPathLoop(GObj* obj, f32 start, f32 end, f32 dt, f32 yawStep, u32 flags);
 /* 0x80362E5C */ void spawnAnimalUsingDeltaHeight(s32 gObjID, u16 id, roomGFX* roomA, roomGFX* roomB, objectSpawn* spawn, animalInitData* initData);
 
 #endif
---
+++ src/volcano_code_72F590.c
@@ -0,0 +1,58 @@
+#include "common.h"
+
+void func_802DE3FC(GObj* obj);
+void func_802DE450(GObj* obj);
+void func_802DE4C0(GObj* obj);
+
+extern animationHeader D_802E31F4;
+extern animationHeader D_802E3208;
+extern animationHeader D_802E321C;
+extern animalInitData koffingSmokeData;
+
+void func_802DE390(GObj* obj) {
+    s32 pad[3];
+    animal* animal = obj->data.animal;
+
+    forceAnimalAnimation(obj, &D_802E31F4);
+    runPathProcess(obj, func_802DE4C0);
+    animal->transitionGraph = NULL;
+    runInteractionsAndWaitForFlags(obj, 1);
+    updateAnimalState(obj, func_802DE3FC);
+}
+
+void func_802DE3FC(GObj* obj) {
+    s32 pad[3];
+    animal* animal = obj->data.animal;
+
+    forceAnimalAnimation(obj, &D_802E3208);
+    animal->transitionGraph = NULL;
+    runInteractionsAndWaitForFlags(obj, 1);
+    updateAnimalState(obj, func_802DE450);
+}
+
+void func_802DE450(GObj* obj) {
+    s32 pad[3];
+    animal* animal = obj->data.animal;
+
+    setAnimalAnimation(obj, &D_802E321C);
+    animal->transitionGraph = NULL;
+    runInteractionsAndWaitForFlags(obj, 1);
+    sendSignalToLink(3, 0x21, obj);
+    runAnimalCleanup(obj);
+    updateAnimalState(obj, NULL);
+}
+
+void func_802DE4C0(GObj* obj) {
+    s32 pad[3];
+    animal* animal = obj->data.animal;
+
+    setNodePosToNegRoom(obj);
+    animalPathLoop(obj, 0, 1.0f, 0.05f, 0.0f, 2);
+    animal->pathProcess = NULL;
+    animal->processFlags |= 2;
+    endGObjProcess(NULL);
+}
+
+void func_802DE52C(s32 gObjID, u16 id, roomGFX* roomA, roomGFX* roomB, objectSpawn* spawn) {
+    spawnAnimalUsingDeltaHeight(gObjID, id, roomA, roomB, spawn, &koffingSmokeData);
+}

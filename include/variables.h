eb4d28f47b499aed469f1bfd78bf6bb3c2796a0f
Ethan Roseman <ethteck@gmail.com>
2020-08-07 19:51:44 -0400

    adding cart var

--- include/variables.h
+++ include/variables.h
@@ -7,6 +7,6 @@
 /* 0x80042CB8 */ extern OSThread* __osRunQueue;
 /* 0x800AC0C0 */ extern s32 gLevelID;
 /* 0x800AC0C4 */ extern s32 gLevelNames[6];
-
+/* 0x803937F0 */ extern GObj* cartGObj;
 
 #endif

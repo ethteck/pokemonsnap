80cea8bc9ed25da988b5e93f78552500bb308f0b
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2024-04-22 11:57:16 +0200

    Match func_8009AE0C (#25)

--- src/46270.c
+++ src/46270.c
@@ -13,20 +13,32 @@ extern Addr stage3_extra_ROM_START, stage3_extra_ROM_END, stage3_extra_VRAM; // 
 extern Addr stage2_extra_ROM_START, stage2_extra_ROM_END, stage2_extra_VRAM; // 3d0560
 extern Addr stage5_extra_ROM_START, stage5_extra_ROM_END, stage5_extra_VRAM; // 47cf30
 extern Addr stage6_extra_ROM_START, stage6_extra_ROM_END, stage6_extra_VRAM; // 4ec000
+extern Addr magikarp3_ROM_START, magikarp3_ROM_END, magikarp3_VRAM;          // 54B5D0
+extern Addr pikachu3_ROM_START, pikachu3_ROM_END, pikachu3_VRAM;             // 54D6A0
+extern Addr zubat3_ROM_START, zubat3_ROM_END, zubat3_VRAM;                   // 554130
+extern Addr bulbasaur3_ROM_START, bulbasaur3_ROM_END, bulbasaur3_VRAM;       // 557050
 extern Addr magikarp2_ROM_START, magikarp2_ROM_END, magikarp2_VRAM;          // 82f8e0
 extern Addr pikachu2_ROM_START, pikachu2_ROM_END, pikachu2_VRAM;             // 832960
 extern Addr zubat2_ROM_START, zubat2_ROM_END, zubat2_VRAM;                   // 837360
 extern Addr bulbasaur2_ROM_START, bulbasaur2_ROM_END, bulbasaur2_VRAM;       // 83a1e0
 
 extern Overlay D_800ABBD0;
+extern Overlay D_800ABDC8;
 extern Overlay D_800ABDEC;
 extern Overlay D_800ABE10;
+extern Overlay D_800ABE34;
 extern Overlay D_800ABE58;
-extern Overlay D_800ABF30;
-extern Overlay D_800ABEE8;
+extern Overlay D_800ABE7C;
 extern Overlay D_800ABEA0;
+extern Overlay D_800ABEC4;
+extern Overlay D_800ABEE8;
+extern Overlay D_800ABF0C;
+extern Overlay D_800ABF30;
+extern Overlay D_800ABF54;
 extern Overlay D_800ABF78;
+extern Overlay D_800ABF9C;
 extern Overlay D_800ABFC0;
+extern Overlay D_800ABFE4;
 extern s32 D_800AC00C;
 extern s32 D_800B0578;
 
@@ -107,7 +119,68 @@ void func_8009A8F0(int param_1) {
     }
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/46270/func_8009AE0C.s")
+void func_8009AE0C(u32 arg0) {
+    dmaLoadOverlay(&D_800ABDEC);
+    dmaLoadOverlay(&D_800ABDC8);
+    switch (arg0) {
+        case 0:
+            dmaLoadOverlay(&D_800ABE34);
+            dmaLoadOverlay(&D_800ABE10);
+            dmaReadRom(magikarp1_ROM_START, magikarp1_VRAM, magikarp1_ROM_END - magikarp1_ROM_START);
+            dmaReadRom(magikarp3_ROM_START, magikarp3_VRAM, magikarp3_ROM_END - magikarp3_ROM_START);
+            dmaReadRom(pikachu1_ROM_START, pikachu1_VRAM, pikachu1_ROM_END - pikachu1_ROM_START);
+            dmaReadRom(pikachu3_ROM_START, pikachu3_VRAM, pikachu3_ROM_END - pikachu3_ROM_START);
+            break;
+        case 1:
+            dmaLoadOverlay(&D_800ABE7C);
+            dmaLoadOverlay(&D_800ABE58);
+            dmaReadRom(magikarp1_ROM_START, magikarp1_VRAM, magikarp1_ROM_END - magikarp1_ROM_START);
+            dmaReadRom(magikarp3_ROM_START, magikarp3_VRAM, magikarp3_ROM_END - magikarp3_ROM_START);
+            dmaReadRom(pikachu1_ROM_START, pikachu1_VRAM, pikachu1_ROM_END - pikachu1_ROM_START);
+            dmaReadRom(pikachu3_ROM_START, pikachu3_VRAM, pikachu3_ROM_END - pikachu3_ROM_START);
+            dmaReadRom(zubat1_ROM_START, zubat1_VRAM, zubat1_ROM_END - zubat1_ROM_START);
+            dmaReadRom(zubat3_ROM_START, zubat3_VRAM, zubat3_ROM_END - zubat3_ROM_START);
+            break;
+        case 4:
+            dmaLoadOverlay(&D_800ABEC4);
+            dmaLoadOverlay(&D_800ABEA0);
+            dmaReadRom(bulbasaur1_ROM_START, bulbasaur1_VRAM, bulbasaur1_ROM_END - bulbasaur1_ROM_START);
+            dmaReadRom(bulbasaur3_ROM_START, bulbasaur3_VRAM, bulbasaur3_ROM_END - bulbasaur3_ROM_START);
+            dmaReadRom(magikarp1_ROM_START, magikarp1_VRAM, magikarp1_ROM_END - magikarp1_ROM_START);
+            dmaReadRom(magikarp3_ROM_START, magikarp3_VRAM, magikarp3_ROM_END - magikarp3_ROM_START);
+            dmaReadRom(pikachu1_ROM_START, pikachu1_VRAM, pikachu1_ROM_END - pikachu1_ROM_START);
+            dmaReadRom(pikachu3_ROM_START, pikachu3_VRAM, pikachu3_ROM_END - pikachu3_ROM_START);
+            dmaReadRom(zubat1_ROM_START, zubat1_VRAM, zubat1_ROM_END - zubat1_ROM_START);
+            dmaReadRom(zubat3_ROM_START, zubat3_VRAM, zubat3_ROM_END - zubat3_ROM_START);
+            break;
+        case 3:
+            dmaLoadOverlay(&D_800ABF0C);
+            dmaLoadOverlay(&D_800ABEE8);
+            dmaReadRom(bulbasaur1_ROM_START, bulbasaur1_VRAM, bulbasaur1_ROM_END - bulbasaur1_ROM_START);
+            dmaReadRom(bulbasaur3_ROM_START, bulbasaur3_VRAM, bulbasaur3_ROM_END - bulbasaur3_ROM_START);
+            dmaReadRom(magikarp1_ROM_START, magikarp1_VRAM, magikarp1_ROM_END - magikarp1_ROM_START);
+            dmaReadRom(magikarp3_ROM_START, magikarp3_VRAM, magikarp3_ROM_END - magikarp3_ROM_START);
+            dmaReadRom(pikachu1_ROM_START, pikachu1_VRAM, pikachu1_ROM_END - pikachu1_ROM_START);
+            dmaReadRom(pikachu3_ROM_START, pikachu3_VRAM, pikachu3_ROM_END - pikachu3_ROM_START);
+            break;
+        case 2:
+            dmaLoadOverlay(&D_800ABF54);
+            dmaLoadOverlay(&D_800ABF30);
+            dmaReadRom(magikarp1_ROM_START, magikarp1_VRAM, magikarp1_ROM_END - magikarp1_ROM_START);
+            dmaReadRom(magikarp3_ROM_START, magikarp3_VRAM, magikarp3_ROM_END - magikarp3_ROM_START);
+            break;
+        case 5:
+            dmaLoadOverlay(&D_800ABF9C);
+            dmaLoadOverlay(&D_800ABF78);
+            dmaReadRom(magikarp1_ROM_START, magikarp1_VRAM, magikarp1_ROM_END - magikarp1_ROM_START);
+            dmaReadRom(magikarp3_ROM_START, magikarp3_VRAM, magikarp3_ROM_END - magikarp3_ROM_START);
+            break;
+        case 6:
+            dmaLoadOverlay(&D_800ABFE4);
+            dmaLoadOverlay(&D_800ABFC0);
+            break;
+    }
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/46270/func_8009B2BC.s")
 

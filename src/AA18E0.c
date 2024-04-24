a6273543cb252a3131b2c4da28df0d8bf2003cd1
Ryan Myers <ryan.p.myers@gmail.com>
2024-04-24 11:28:13 -0400

    Get func_801DC9D0_AA1A10 to NON_MATCHING status

--- src/AA18E0.c
+++ src/AA18E0.c
@@ -1,20 +1,23 @@
 #include "common.h"
 #include "sys/oh.h"
 
-void func_8009FA68(void* arg0, s32* arg1);
+void func_8009FA68(UNK_PTR arg0, UNK_PTR arg1);
 void func_801DC9D0_AA1A10(GObj*);
+void func_8009FB50(u8 arg0, u8 arg1, u8 arg2);
+UNK_PTR func_800BF574_5C414(s32 arg0);
 
 // data
 extern Sprite D_801E5478_AAA4B8;
 extern u8 D_801E54BC_AAA4FC;
 extern u8 D_801E54C0_AAA500;
 extern u8 D_801E54C4_AAA504;
-extern s32 D_801E54C8_AAA508;
+extern s32 D_801E54C8_AAA508[211]; // All 0xBDEFBDEF
 extern s32 D_801E550C_AAA54C;
 extern ScreenSettings D_801E5510_AAA550;
 extern SceneSetup D_801E552C_AAA56C;
 extern s32 D_801E553C_AAA57C;
 extern f32 D_801E55C0_AAA600;
+extern UNK_PTR D_801E54CC_AAA50C;
 
 // bss
 s32 D_801E55D0_AAA610[2];
@@ -32,14 +35,14 @@ void func_801DC8A0_AA18E0(s32 arg0) {
         }
         contPrinterSendCommand(0x10);
     }
-    D_801E54C8_AAA508 = arg0;
+    D_801E54C8_AAA508[0] = arg0;
     if (arg0 == 0) {
         func_8009FBC4();
     }
     func_800067DC();
 }
 
-void func_801DC930_AA1970(void* arg0, s32* arg1) {
+void func_801DC930_AA1970(UNK_PTR arg0, UNK_PTR arg1) {
     GObj* obj;
 
     func_8009FA68(arg0, arg1);
@@ -55,11 +58,93 @@ void func_801DC930_AA1970(void* arg0, s32* arg1) {
     }
 }
 
-void func_801DC9BC_AA19FC(s32 unused) {
+void func_801DC9BC_AA19FC(void *unused) {
     D_801E550C_AAA54C = 1;
 }
 
+#ifdef NON_MATCHING
+// Single stack diff from matching
+void func_801DC9D0_AA1A10(GObj* arg0) {
+    void* sp2C;
+    s32 i;
+    s32 var_a0;
+    s32 var_s0;
+    s32 *temp_v0;
+
+    sp2C = arg0->data.any;
+    if (D_801E54C8_AAA508[0] >= 0) {
+        func_800067DC();
+    }
+    var_s0 = 0;
+    for (var_a0 = 0; var_a0 < 4; var_a0++) {
+        temp_v0 = func_800BF574_5C414(var_a0);
+        if (temp_v0 != NULL) {
+            var_s0++;
+            D_801E55E0_AAA620[var_a0] = temp_v0;
+        }
+
+    }
+    if (var_s0 < 4) {
+        func_801DC8A0_AA18E0(2);
+        ohWait(1);
+    }
+    if (D_801E55D8_AAA618 == 3) {
+        D_801E55DC_AAA61C->flags &= ~1;
+    } else {
+        D_801E55DC_AAA61C->flags |= 1;
+    }
+    if (D_801E55D8_AAA618 >= 0 && D_801E55D8_AAA618 < 16) {
+        func_8009FB50(D_801E54BC_AAA4FC, D_801E54C0_AAA500, D_801E54C4_AAA504);
+        var_s0 = D_801E55E0_AAA620[D_801E54C8_AAA508[1 + D_801E55D8_AAA618]];
+        D_801E550C_AAA54C = 0;
+        scSetPostProcessFunc(func_801DC9BC_AA19FC);
+        func_801DC930_AA1970(sp2C, var_s0);
+        if (D_801E55D8_AAA618 == 0) {
+            while (contPrinterGetStatus() == 8) {
+                ohWait(1);
+            }
+            contPrinterSendCommand(1);
+            while (contPrinterGetStatus() == 8) {
+                ohWait(1);
+            }
+        }
+        for (i = 0; i < 180; i++) {
+            if (D_801E550C_AAA54C == 0) {
+                ohWait(1);
+            } else {
+                break;
+            }
+        }
+        scRemovePostProcessFunc();
+        ohWait(1);
+        contPrinterSendCommand(2);
+        while (contPrinterGetStatus() == 8) {
+            ohWait(1);
+        }
+        ohWait(1);
+    }
+    if (D_801E55D8_AAA618 < 16) {
+        func_801DC8A0_AA18E0(0);
+        ohWait(1);
+    }
+    contPrinterSendCommand(4);
+    while (contPrinterGetStatus() == 8) {
+        ohWait(1);
+    }
+    ohWait(1);
+    while (1) {
+        if (gContInputPressedButtons & START_BUTTON) {
+            contPrinterSendCommand(4);
+            while (contPrinterGetStatus() == 8) {
+                ohWait(1);
+            }
+        }
+        ohWait(1);
+    }
+}
+#else
 #pragma GLOBAL_ASM("asm/nonmatchings/AA18E0/func_801DC9D0_AA1A10.s")
+#endif
 
 void func_801DCCD4_AA1D14(s32 unused) {
 }
@@ -115,5 +200,5 @@ s32 func_801DD010_AA2050(s32 arg0) {
     D_801E553C_AAA57C = 0x2D4000;
     gtlDisableNearClipping(1);
     omSetupScene(&D_801E552C_AAA56C);
-    return D_801E54C8_AAA508;
+    return D_801E54C8_AAA508[0];
 }

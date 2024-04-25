6a15c2b6b6c1bc217938bb241338021d11f58536
angie <angheloalf95@gmail.com>
2024-04-25 15:33:55 -0400

    volcano_code rodata

---
+++ src/72EE30.c
@@ -0,0 +1,11 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/72EE30/func_802DDC30_72EE30.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/72EE30/func_802DDCA0_72EEA0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/72EE30/func_802DDD04_72EF04.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/72EE30/func_802DDE00_72F000.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/72EE30/func_802DDEC0_72F0C0.s")

154fd4f41046e86807e9724892ee2d09fdcae589
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2024-04-26 02:39:11 +0200

    Continue

--- src/credits/A937C0.c
+++ src/credits/A937C0.c
@@ -1,38 +1,288 @@
 #include "common.h"
 
-#pragma GLOBAL_ASM("asm/nonmatchings/credits/A937C0/func_credits_801DCC00.s")
+typedef struct UnkChartreuseAngler {
+    /* 0x00 */ s32 unk0;
+    /* 0x04 */ s32 unk4;
+    /* 0x08 */ void* unk8;
+    /* 0x0C */ s32 unkC;
+} UnkChartreuseAngler; // size: 0x80
 
-#pragma GLOBAL_ASM("asm/nonmatchings/credits/A937C0/func_credits_801DCC70.s")
+typedef struct UnkCitronCod {
+    /* 0x00 */ u8 pad0[0x44];
+    /* 0x44 */ UnkChartreuseAngler* unk44;
+} UnkCitronCod;
 
-#pragma GLOBAL_ASM("asm/nonmatchings/credits/A937C0/func_credits_801DCCE4.s")
+extern Sprite D_credits_801E8B50;
+extern GObj* D_credits_801ECCA0;
+extern GObj* D_credits_801ECC80;
+extern GObj* D_credits_801ECC98;
+extern GObj* D_credits_801ECC9C;
 
-#pragma GLOBAL_ASM("asm/nonmatchings/credits/A937C0/func_credits_801DCD60.s")
+extern u8 D_credits_801ECDA4;
+extern s32 D_credits_801ED010;
 
-#pragma GLOBAL_ASM("asm/nonmatchings/credits/A937C0/func_credits_801DCD80.s")
+void func_credits_801DCAF8(SObj*); /* extern */
+GObj* func_credits_801DCC00(GObj*, Sprite*);
+void func_credits_801DD8C0(SObj*);
+void func_credits_801DEB74(void*, float);
+void func_credits_801DEA30(void*, float);
+void func_credits_801DCEF0(GObj*);
+void func_credits_801DE8A0(SObj*, s32);
+s32 func_credits_801DE8D8();
+void func_credits_801DD540(GObj*); /* extern */
 
-#pragma GLOBAL_ASM("asm/nonmatchings/credits/A937C0/func_credits_801DCE10.s")
+GObj* func_credits_801DCC00(GObj* arg0, Sprite* arg1) {
+    return ohCreateSprite(0xE, ohUpdateDefault, 0, 0x80000000, renDrawSprite, 1U, 0x80000000, -1, arg1, 0U, NULL, 1);
+}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/credits/A937C0/func_credits_801DCEF0.s")
+extern Sprite D_credits_801E6058;
+extern Sprite D_credits_801E6430;
+extern Sprite D_credits_801E67D8;
 
-#pragma GLOBAL_ASM("asm/nonmatchings/credits/A937C0/func_credits_801DCF94.s")
+GObj* func_credits_801DCC70(void) {
+    GObj* temp_v0;
 
-#pragma GLOBAL_ASM("asm/nonmatchings/credits/A937C0/func_credits_801DCFC8.s")
+    temp_v0 = func_credits_801DCC00(temp_v0, &D_credits_801E6058);
+    omGObjAddSprite(temp_v0, &D_credits_801E6430);
+    omGObjAddSprite(temp_v0, &D_credits_801E6430);
+    omGObjAddSprite(temp_v0, &D_credits_801E67D8);
+    omGObjAddSprite(temp_v0, &D_credits_801E67D8);
 
-#pragma GLOBAL_ASM("asm/nonmatchings/credits/A937C0/func_credits_801DD018.s")
+    return temp_v0;
+}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/credits/A937C0/func_credits_801DD12C.s")
+void func_credits_801DCCE4(SObj* arg0) {
+    SObj* temp_v0;
 
-#pragma GLOBAL_ASM("asm/nonmatchings/credits/A937C0/func_credits_801DD168.s")
+    temp_v0 = arg0;
+    temp_v0->sprite.x = 0x5A;
+    temp_v0->sprite.y = 0x44;
 
-#pragma GLOBAL_ASM("asm/nonmatchings/credits/A937C0/func_credits_801DD258.s")
+    temp_v0 = arg0->next;
+    temp_v0->sprite.x = arg0->sprite.x - 3;
+    temp_v0->sprite.y = arg0->sprite.y - 3;
 
-#pragma GLOBAL_ASM("asm/nonmatchings/credits/A937C0/func_credits_801DD2D4.s")
+    temp_v0 = temp_v0->next;
+    temp_v0->sprite.x = arg0->sprite.x + 0x8C;
+    temp_v0->sprite.y = arg0->sprite.y - 3;
 
-#pragma GLOBAL_ASM("asm/nonmatchings/credits/A937C0/func_credits_801DD320.s")
+    temp_v0 = temp_v0->next;
+    temp_v0->sprite.x = arg0->sprite.x;
+    temp_v0->sprite.y = arg0->sprite.y - 3;
 
-#pragma GLOBAL_ASM("asm/nonmatchings/credits/A937C0/func_credits_801DD340.s")
+    temp_v0 = temp_v0->next;
+    temp_v0->sprite.x = arg0->sprite.x;
+    temp_v0->sprite.y = arg0->sprite.y + 0x69;
+}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/credits/A937C0/func_credits_801DD3E0.s")
+void func_credits_801DCD60(SObj* arg0) {
+    while (arg0) {
+        arg0->sprite.attr = SP_TEXSHUF | SP_SCALE | SP_HIDDEN | SP_TRANSPARENT;
+        arg0 = arg0->next;
+    }
+}
+
+void func_credits_801DCD80(SObj* arg0) {
+    Bitmap* list;
+    u16* buf;
+    u8 var_s0;
+
+    list = (Bitmap*) func_8036A194_83D944(sizeof(Bitmap) * 8);
+    for (var_s0 = 0; var_s0 < 8u; var_s0++) {
+        buf = func_8036A194_83D944(14 * 140 * sizeof(u16));
+
+        list[var_s0] = arg0->sprite.bitmap[var_s0];
+        list[var_s0].buf = buf;
+    }
+
+    arg0->sprite.bitmap = list;
+}
+
+void func_credits_801DCE10(void) {
+    GObj* temp_v0;
+    SObj* sobj;
+    s8 var_s2;
+
+    for (var_s2 = 4; var_s2 >= 0; var_s2--) {
+        temp_v0 = func_credits_801DCC70();
+        (&D_credits_801ECC80)[var_s2] = temp_v0;
+        sobj = temp_v0->data.sobj;
+        func_credits_801DCCE4(sobj);
+        func_credits_801DCD60(sobj);
+        func_credits_801DCD80(sobj);
+        func_credits_801DE8A0(sobj, func_credits_801DE8D8());
+        func_credits_801DEB74(sobj, 4.0f);
+        func_credits_801DEA30(sobj, 4.0f);
+        while (sobj != NULL) {
+            sobj->sprite.attr &= ~SP_HIDDEN;
+            sobj = sobj->next;
+        }
+    }
+}
+
+void func_credits_801DCEF0(GObj* arg0) {
+    ohWait(0xE2);
+
+    while (1) {
+        if (gContInputPressedButtons & 0x9000) {
+            func_800A7470(0, 0, 0);
+            func_800A7860(0, 1.0f);
+            auSetBGMVolumeSmooth(0, 0U, 0x3CU);
+            ohWait(0x3C);
+            func_800067DC();
+
+            while (1) {
+                ohWait(1);
+            }
+        }
+        ohWait(1);
+    }
+}
+
+void func_credits_801DCF94(void) {
+    omCreateProcess(D_credits_801ECC80, func_credits_801DCEF0, 0U, 1U);
+}
+
+s32 func_credits_801DCFC8(GObj* arg0) {
+    SObj* temp_v0;
+
+    temp_v0 = arg0->data.sobj;
+    if (temp_v0->sprite.attr & SP_HIDDEN) {
+        if ((temp_v0->next->sprite.attr & SP_HIDDEN) && (temp_v0->next->next->sprite.attr & SP_HIDDEN)) {
+            return 1;
+        }
+    }
+
+    return 0;
+}
+
+void func_credits_801DD018(GObj* arg0) {
+    SObj* var_s0;
+
+    var_s0 = arg0->data.sobj;
+    while (var_s0 != NULL) {
+        var_s0->sprite.x -= 1;
+        if (var_s0->sprite.x < -259.0) {
+            if (D_credits_801ED010 != 0) {
+                D_credits_801ED010--;
+                if (D_credits_801ED010 == 0) {
+                    D_credits_801ECDA4 = 2;
+                }
+            }
+            if (D_credits_801ECDA4 == 1) {
+                var_s0->sprite.x += 700.0;
+                func_credits_801DCAF8(var_s0);
+            } else {
+                var_s0->sprite.attr |= SP_HIDDEN;
+            }
+        }
+
+        var_s0 = var_s0->next;
+    }
+}
+
+s32 func_credits_801DD12C(GObj* arg0) {
+    SObj* sobj;
+
+    sobj = arg0->data.sobj;
+    while (sobj != NULL) {
+        if (sobj->sprite.x == 90) {
+            return 90;
+        }
+        sobj = sobj->next;
+    }
+
+    return 0;
+}
+
+void func_credits_801DD168(GObj* arg0) {
+    s32 var_s0;
+
+    ohWait(1);
+    while (D_credits_801ECDA4 != 1) {
+        ohWait(1);
+    }
+
+    var_s0 = 0;
+    while (1) {
+        while (var_s0 != 0) {
+            ohWait(1);
+            var_s0 = (var_s0 - 1) & 0xFF;
+        }
+
+        if (func_credits_801DCFC8(arg0)) {
+            break;
+        }
+
+        func_credits_801DD018(arg0);
+        var_s0 = func_credits_801DD12C(arg0) & 0xFF;
+        ohWait(1);
+    }
+
+    ohRemoveSprite(arg0);
+
+    while (1) {
+        ohWait(1);
+    }
+}
+
+GObj* func_credits_801DD258(void) {
+    GObj* temp_v0;
+
+    temp_v0 = func_credits_801DCC00(temp_v0, &D_credits_801E6058);
+    omGObjAddSprite(temp_v0, &D_credits_801E6058);
+    omGObjAddSprite(temp_v0, &D_credits_801E6058);
+    omGObjAddSprite(temp_v0, &D_credits_801E6058);
+    omCreateProcess(temp_v0, func_credits_801DD168, 0U, 1U);
+
+    return temp_v0;
+}
+
+void func_credits_801DD2D4(SObj* arg0) {
+    s16 var_v0;
+
+    var_v0 = 320;
+    while (arg0 != NULL) {
+        arg0->sprite.x = var_v0;
+        arg0->sprite.y = 68;
+        arg0 = arg0->next;
+        var_v0 += 175.0;
+    }
+}
+
+void func_credits_801DD320(SObj* arg0) {
+    while (arg0) {
+        arg0->sprite.attr = SP_TEXSHUF | SP_HIDDEN | SP_TRANSPARENT;
+        arg0 = arg0->next;
+    }
+}
+
+void func_credits_801DD340(SObj* arg0) {
+    Bitmap* list;
+    u16* buf;
+    u8 i;
+
+    while (arg0 != NULL) {
+        list = (Bitmap *)func_8036A194_83D944(sizeof (Bitmap) * 8);
+
+        for (i = 0; i < 8u; i++) {
+            buf = func_8036A194_83D944(1960 * sizeof(u16));
+
+            list[i] = arg0->sprite.bitmap[i];
+            list[i].buf = buf;
+        }
+
+        arg0->sprite.bitmap = list;
+        arg0 = arg0->next;
+    }
+}
+
+void func_credits_801DD3E0(SObj* arg0) {
+    while (arg0 != NULL) {
+        func_credits_801DE8A0(arg0, func_credits_801DE8D8());
+        arg0 = arg0->next;
+    }
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/credits/A937C0/func_credits_801DD428.s")
 
@@ -40,21 +290,63 @@
 
 #pragma GLOBAL_ASM("asm/nonmatchings/credits/A937C0/func_credits_801DD540.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/credits/A937C0/func_credits_801DD744.s")
+void func_credits_801DD744(void) {
+    ohEndAllObjectProcesses(D_credits_801ECC98);
+    ohEndAllObjectProcesses(D_credits_801ECC9C);
+    omCreateProcess(D_credits_801ECC98, func_credits_801DD540, 0U, 1U);
+}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/credits/A937C0/func_credits_801DD790.s")
+void func_credits_801DD49C(GObj*); /* extern */
+extern Sprite D_credits_801E6AA8;
 
-#pragma GLOBAL_ASM("asm/nonmatchings/credits/A937C0/func_credits_801DD7D8.s")
+GObj* func_credits_801DD790(void) {
+    GObj* sp1C;
 
-#pragma GLOBAL_ASM("asm/nonmatchings/credits/A937C0/func_credits_801DD83C.s")
+    sp1C = func_credits_801DCC00(sp1C, &D_credits_801E6AA8);
+    omCreateProcess(sp1C, func_credits_801DD49C, 0U, 1U);
+    return sp1C;
+}
+
+void func_credits_801DD7D8(SObj* arg0, u8 arg1) {
+    arg0->sprite.width = 0x150;
+
+    switch (arg1) { /* irregular */
+        case 0:
+            // clang-format off
+            // TODO: look into setPos(x, y) macro
+            arg0->sprite.x = 0x140; arg0->sprite.y = 0x44 - (arg0->sprite.height * 2);
+            // clang-format on
+            return;
+        case 1:
+            arg0->sprite.x = 0x140;
+            arg0->sprite.y = arg0->sprite.height + 0xAC;
+            return;
+    }
+}
+
+void func_credits_801DD83C(SObj* arg0) {
+    arg0->sprite.attr = 0x201;
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/credits/A937C0/func_credits_801DD848.s")
 
 #pragma GLOBAL_ASM("asm/nonmatchings/credits/A937C0/func_credits_801DD8C0.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/credits/A937C0/func_credits_801DD930.s")
+void func_credits_801DD930(SObj* arg0) {
+    arg0->sprite.attr = 0x201;
+}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/credits/A937C0/func_credits_801DD93C.s")
+void func_credits_801DD93C(void) {
+    GObj* sp1C;
+    SObj* temp_a0;
+    GObj* temp_v0;
+
+    temp_v0 = func_credits_801DCC00(sp1C, &D_credits_801E8B50);
+    D_credits_801ECCA0 = temp_v0;
+    temp_a0 = temp_v0->data.sobj;
+    func_credits_801DD8C0(temp_a0);
+    func_credits_801DD930(temp_a0);
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/credits/A937C0/func_credits_801DD980.s")
 
--- src/credits/A94940.c
+++ src/credits/A94940.c
@@ -1,29 +1,218 @@
 #include "common.h"
 
-#pragma GLOBAL_ASM("asm/nonmatchings/credits/A94940/func_credits_801DDD80.s")
+typedef struct UnkTealGrouper UnkTealGrouper;
+struct UnkTealGrouper {
+    u8 pad_00[0x8];
+    UnkTealGrouper* var_8;
+    u8 pad_C[0xC];
+    f32 var_18;
+    f32 var_1C;
+};
 
+extern s32 D_credits_801ECDA0;
+extern u8 D_credits_801ECDA3;
+extern s32 D_credits_801ECDA8;
+extern s32 D_credits_801ECDB0[];
+
+void func_80374714(void*, s32);
+void func_credits_801DDF48(GObj*);
+void func_credits_801DDE24(GObj*);
+void func_credits_801DDAE8(u8);
+void func_credits_801DDC70(void);
+void func_credits_801DDD80(void);
+void func_credits_801DE734(s32);
+void func_credits_801DE7B4(s32);
+void func_credits_801DCF94(void);
+void func_credits_801DE060(void);
+void func_credits_801DE35C(void);
+
+void func_credits_801DDD80(void) {
+    s32* var_v0;
+    u32 var_s0;
+
+    D_credits_801ECDA8 = 0;
+    var_v0 = D_credits_801ECDB0;
+    for (var_s0 = 0; var_s0 < 0x98; var_s0++) {
+        *var_v0++ = 0x3E7;
+    }
+
+    for (var_s0 = 0; var_s0 < 0x98; var_s0++) {
+        if (func_800BF710_5C5B0(var_s0) != NULL) {
+            D_credits_801ECDB0[D_credits_801ECDA8] = var_s0;
+            D_credits_801ECDA8 += 1;
+        }
+    }
+}
+
+#if 0
+// TODO: This should match when rodata is moved
+void func_credits_801DEA30(SObj*, f32); /* extern */
+void func_credits_801DEB74(SObj*, f32); /* extern */
+extern f32 D_credits_801E9A84; // 0.1000000015
+void func_credits_801DDE24(GObj* arg0) {
+    SObj* temp_s0;
+    f32 scale;
+    f32 temp_f22;
+
+    temp_s0 = arg0->data.sobj;
+    auPlaySoundWithParams(0x47U, 0x7FFF, 0x40, 1.0, 0xF);
+    temp_f22 = 0.1000000015;
+
+    scale = 4.0f;
+    while (temp_s0->sprite.scalex > 1.0f) {
+        scale -= temp_f22;
+        ohWait(1);
+        func_credits_801DEB74(temp_s0, scale);
+        func_credits_801DEA30(temp_s0, scale);
+    }
+    func_credits_801DEA30(temp_s0, 1.0);
+    func_credits_801DEB74(temp_s0, 1.0);
+    ohWait(1);
+    omEndProcess(NULL);
+
+    while (1) {
+        ohWait(1);
+    }
+}
+#else
 #pragma GLOBAL_ASM("asm/nonmatchings/credits/A94940/func_credits_801DDE24.s")
+#endif
 
-#pragma GLOBAL_ASM("asm/nonmatchings/credits/A94940/func_credits_801DDF1C.s")
+void func_credits_801DDF1C(GObj* arg0) {
+    omCreateProcess(arg0, func_credits_801DDE24, 0U, 1U);
+}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/credits/A94940/func_credits_801DDF48.s")
+void func_credits_801DDF48(GObj* arg0) {
+    SObj* temp_s1;
+    SObj* var_v0;
+    s16 var_s0;
 
-#pragma GLOBAL_ASM("asm/nonmatchings/credits/A94940/func_credits_801DE034.s")
+    temp_s1 = arg0->data.sobj;
+    auPlaySoundWithParams(0x50U, 0x7FFF, 0x40, 1.0f, 0xF);
+
+    var_s0 = 0xA;
+    while (temp_s1->sprite.y < 0x12C) {
+        for (var_v0 = temp_s1; var_v0 != NULL; var_v0 = var_v0->next) {
+            var_v0->sprite.y += var_s0;
+        }
+
+        var_s0++;
+        ohWait(1);
+    }
+
+    for (var_v0 = temp_s1; var_v0 != NULL; var_v0 = var_v0->next) {
+        var_v0->sprite.attr |= 4;
+    }
+    omEndProcess(NULL);
+
+    while (1) {
+        ohWait(1);
+    }
+}
+
+void func_credits_801DE034(GObj* arg0) {
+    omCreateProcess(arg0, func_credits_801DDF48, 0U, 1U);
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/credits/A94940/func_credits_801DE060.s")
 
 #pragma GLOBAL_ASM("asm/nonmatchings/credits/A94940/func_credits_801DE35C.s")
 
-#pragma GLOBAL_ASM("asm/nonmatchings/credits/A94940/func_credits_801DE734.s")
+void func_credits_801DE734(s32 arg0) {
+    switch (D_credits_801ECDA0) {
+        case 18:
+            func_credits_801DE060();
+            return;
+        case 17:
+        case 20:
+            func_credits_801DCF94();
+            func_credits_801DE060();
+            return;
+        case 19:
+            func_credits_801DE35C();
+            return;
+    }
+}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/credits/A94940/func_credits_801DE7B4.s")
+void func_credits_801DE7B4(s32 arg0) {
+    auSetBGMVolume(0, 0x7F00);
+    switch (arg0) {
+        case 18:
+            auPlaySong(0, 0xB);
+            return;
+        case 19:
+            auPlaySong(0, 0x24);
+            return;
+        case 17:
+        case 20:
+            auPlaySong(0, 0xB);
+            return;
+    }
+}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/credits/A94940/func_credits_801DE844.s")
+void func_credits_801DE844(s32 arg0) {
+    func_credits_801DE7B4(D_credits_801ECDA0);
+    func_credits_801DDD80();
+    func_credits_801DDAE8(D_credits_801ECDA3);
+    func_credits_801DDC70();
+    func_credits_801DE734(arg0);
+    func_800067DC();
+    ohWait(1);
+}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/credits/A94940/func_credits_801DE8A0.s")
+void func_credits_801DE8A0(s32 arg0, s32 arg1) {
+    void* temp_v0;
+
+    temp_v0 = func_800BF710_5C5B0(arg1);
+    if (temp_v0 != NULL) {
+        func_80374714(temp_v0, arg0 + 0x10);
+    }
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/credits/A94940/func_credits_801DE8D8.s")
 
+#if 1
 #pragma GLOBAL_ASM("asm/nonmatchings/credits/A94940/func_credits_801DEA30.s")
+#else
+void func_credits_801DEA30(SObj* arg0, f32 arg1) {
+    SObj* temp_v1;
+    f32 temp_f0;
+    s16 temp_f10;
+    s16 temp_f6;
+    f32 i;
 
-#pragma GLOBAL_ASM("asm/nonmatchings/credits/A94940/func_credits_801DEB74.s")
+    temp_f0 = 70.0f * arg1;
+    arg0->sprite.x = (s16) (s32) (160.0f - temp_f0);
+    arg0->sprite.y = (s16) (s32) (120.0f - (52.0f * arg1));
+    temp_f10 = (255.0f - ((arg1 - 1.0f) * 53.0f));
+    temp_f6 = (3.0f * arg1);
+    arg0->sprite.blue = temp_f10;
+    arg0->sprite.green = temp_f10;
+    arg0->sprite.red = temp_f10;
+
+    temp_v1 = arg0->next;
+    temp_v1->sprite.x = arg0->sprite.x - temp_f6;
+    temp_v1->sprite.y = arg0->sprite.y - temp_f6;
+
+    temp_v1 = temp_v1->next;
+    temp_v1->sprite.x = arg0->sprite.x + (s32) (140.0f * arg1);
+    temp_v1->sprite.y = arg0->sprite.y - temp_f6;
+
+    temp_v1 = temp_v1->next;
+    i = 0xA0 - temp_f0;
+    temp_v1->sprite.x = i;
+    temp_v1->sprite.y = arg0->sprite.y - temp_f6;
+
+    temp_v1 = temp_v1->next;
+    temp_v1->sprite.x = i;
+    temp_v1->sprite.y = arg0->sprite.y + (s32) (105.0f * arg1);
+}
+#endif
+
+void func_credits_801DEB74(SObj* arg0, f32 arg1) {
+    while (arg0 != NULL) {
+        arg0->sprite.scalex = arg1;
+        arg0->sprite.scaley = arg1;
+        arg0 = arg0->next;
+    }
+}

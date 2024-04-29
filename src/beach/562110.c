b21ceaf2d1025313a4e8fe016ff883613410f8c7
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2024-04-29 13:16:11 +0200

    Match 562110

--- src/beach/562110.c
+++ src/beach/562110.c
@@ -1,15 +1,70 @@
 #include "common.h"
 
-#pragma GLOBAL_ASM("asm/nonmatchings/beach/562110/func_beach_802CA0A0.s")
+extern animalInitData D_beach_802CD8E4;
+extern idFuncStruct D_beach_802CD858;
+extern AnimalDef D_beach_802CD848;
+extern AnimalDef D_beach_802CD838;
+extern randomTransition D_beach_802CD8B8;
 
-#pragma GLOBAL_ASM("asm/nonmatchings/beach/562110/func_beach_802CA0F4.s")
+void func_beach_802CA0A0(GObj* arg0) {
+    // TODO: see if this can be inlined
+    AnimalDef sp18 = D_beach_802CD838;
 
-#pragma GLOBAL_ASM("asm/nonmatchings/beach/562110/func_beach_802CA148.s")
+    addAnimalAtGeo(arg0, 0x10U, &sp18);
+    omEndProcess(NULL);
+}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/beach/562110/func_beach_802CA198.s")
+void func_beach_802CA0F4(GObj* arg0) {
+    // TODO: see if this can be inlined
+    AnimalDef sp18 = D_beach_802CD848;
 
-#pragma GLOBAL_ASM("asm/nonmatchings/beach/562110/func_beach_802CA1BC.s")
+    addAnimalAtGeo(arg0, 0x7BU, &sp18);
+    omEndProcess(NULL);
+}
 
-#pragma GLOBAL_ASM("asm/nonmatchings/beach/562110/func_beach_802CA23C.s")
+void func_beach_802CA148(GObj* obj) {
+    Animal* animal = GET_ANIMAL(obj);
 
-#pragma GLOBAL_ASM("asm/nonmatchings/beach/562110/func_beach_802CA2BC.s")
+    animal->tangible = 0;
+    obj->flags |= 3;
+    animal->transitionGraph = &D_beach_802CD858;
+    runInteractionsAndWaitForFlags(obj, 0U);
+    updateAnimalState(obj, NULL);
+}
+
+void func_beach_802CA198(GObj* arg0) {
+    weightedRandomStaightTransition(arg0, &D_beach_802CD8B8);
+}
+
+void func_beach_802CA1BC(GObj* obj) {
+    UNUSED s32 pad[3];
+    Animal* animal = GET_ANIMAL(obj);
+
+    omCreateProcess(obj, func_beach_802CA0A0, 1U, 1U);
+    // clang-format off
+    animal->counter = 1; animal->processFlags &= ~4;
+    // clang-format on
+
+    animal->transitionGraph = NULL;
+    runInteractionsAndWaitForFlags(obj, 4U);
+    runAnimalCleanup(obj);
+    updateAnimalState(obj, NULL);
+}
+
+void func_beach_802CA23C(GObj* obj) {
+    UNUSED s32 pad[3];
+    Animal* animal = GET_ANIMAL(obj);
+
+    omCreateProcess(obj, func_beach_802CA0F4, 1U, 1U);
+    // clang-format off
+    animal->counter = 1; animal->processFlags &= ~4;
+    // clang-format on
+    animal->transitionGraph = NULL;
+    runInteractionsAndWaitForFlags(obj, 4U);
+    runAnimalCleanup(obj);
+    updateAnimalState(obj, NULL);
+}
+
+void func_beach_802CA2BC(s32 gObjID, u16 id, RoomGFX* roomA, RoomGFX* roomB, ObjectSpawn* spawn) {
+    spawnAnimalUsingDeltaHeight(gObjID, id, roomA, roomB, spawn, &D_beach_802CD8E4);
+}

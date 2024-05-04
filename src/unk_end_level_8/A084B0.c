a56987b2b33db8f15556ea27fb7e3bb1445d0ae5
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2024-05-04 13:19:53 +0200

    Move some unk_env_level segments into dirs

---
+++ src/unk_end_level_8/A084B0.c
@@ -0,0 +1,15 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/unk_end_level_8/A084B0/func_801DC8C0_A084B0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/unk_end_level_8/A084B0/func_801DC9E8_A085D8.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/unk_end_level_8/A084B0/func_801DCE2C_A08A1C.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/unk_end_level_8/A084B0/func_801DCE64_A08A54.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/unk_end_level_8/A084B0/func_801DCFE0_A08BD0.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/unk_end_level_8/A084B0/func_801DD070_A08C60.s")
+
+#pragma GLOBAL_ASM("asm/nonmatchings/unk_end_level_8/A084B0/func_801DD09C_A08C8C.s")

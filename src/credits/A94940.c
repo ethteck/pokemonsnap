989b3f3e378ed3200d57547437adb2daf362ffaa
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2024-05-16 07:44:43 +0200

    Allow empty blocks on a single line

--- src/credits/A94940.c
+++ src/credits/A94940.c
@@ -277,9 +277,7 @@ void func_credits_801DE060(void) {
         if (i == ARRAY_COUNT(D_credits_801E9400) - 1) {
             limit = 234.0f / val;
         } else {
-            if (1) {
-                // required to match
-            }
+            if (1) {} // required to match
             limit = 198.0f / val;
         }
 
@@ -475,9 +473,7 @@ void func_credits_801DEA30(SObj* sobj, f32 arg1) {
     sobj->sprite.x = (SCREEN_WIDTH / 2) - (70.0f * arg1);
     sobj->sprite.y = (SCREEN_HEIGHT / 2) - (52.0f * arg1);
 
-    if (1) {
-        // required to match
-    }
+    if (1) {} // required to match
     sobj->sprite.red = sobj->sprite.green = sobj->sprite.blue = (s16) (255.0f - ((arg1 - 1.0f) * 53.0f));
 
     it = sobj->next;

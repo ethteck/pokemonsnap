cf2bb7002e712105a4bd909ebd455ab6feab7235
Unnunu <serge_zorkin@mail.ru>
2024-08-07 14:12:10 +0300

    matched dmaDecodeVPK0

--- src/app_level/504770.c
+++ src/app_level/504770.c
@@ -11,7 +11,7 @@ extern u8 gSPDmemOkay;
 #define UNK_STUFF_MINUS_8 *(u8*) (UNK_STUFF_VRAM - 0x08)
 
 void func_80364360_504770(void) {
-    dmaReadVPK((u32*) AAA610_ROM_START, UNK_STUFF_VRAM);
+    dmaReadVPK0((u32*) AAA610_ROM_START, UNK_STUFF_VRAM);
     UNK_STUFF_SP_IMEM_OK = gSPImemOkay;
     UNK_STUFF_SP_DMEM_OK = gSPDmemOkay;
     ((void (*)(void)) UNK_STUFF_VRAM)();

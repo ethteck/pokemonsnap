12c16314ed01c110d1d784d7879c35bc53bfad5c
Unnunu <serge_zorkin@mail.ru>
2024-08-21 10:35:40 +0300

    build ok

--- src/volcano/assetswqe.c
+++ src/volcano/assetswqe.c
@@ -1,11 +1,11 @@
 #include "common.h"
 
 static u8 aligner_1[] = { 0, 0, 0, 0, 0, 0, 0, 0 };
-u8 test_image_pal[] = {
+u8 arcanine_tex14_pal[] = {
 #include "assets/volcano/test_image.pal.bin.c"
 };
 
 static u8 aligner_2[] = { 0, 0, 0, 0, 0, 0, 0, 0 };
-u8 test_image_png[] = {
+u8 arcanine_tex14_png[] = {
 #include "assets/volcano/test_image.png.bin.c"
 };

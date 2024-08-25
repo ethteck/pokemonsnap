2480301f9a97fa97132f7b1384f7605a1a66ce76
Ethan Roseman <ethteck@gmail.com>
2024-08-26 01:11:20 +0900

    Fix stuff (#92)
    * wip
    
    * 2big

--- tools/splat_ext/snap_height_map_tree.py
+++ tools/splat_ext/snap_height_map_tree.py
@@ -1,9 +1,9 @@
-from splat.segtypes.n64.segment import N64Segment
+from splat.segtypes.segment import Segment
 from splat.util import options
 from pathlib import Path
 import struct
 
-class N64SegSnap_height_map_tree(N64Segment):
+class N64SegSnap_height_map_tree(Segment):
     def scan(self, rom_bytes):
         data = rom_bytes[self.rom_start : self.rom_end]
         self.file_text = self.disassemble_data(data)
@@ -15,7 +15,7 @@ class N64SegSnap_height_map_tree(N64Segment):
         for entry in struct.iter_unpack(">fffiiii", data):
             A, B, C, leftchild, rightchild, leftpatch, rightpatch = entry
             lines.append(f"{{ {A:12.6f}, {B:12.6f}, {C:12.6f}, (void*){leftchild:5}, (void*){rightchild:5}, (void*){leftpatch:5}, (void*){rightpatch:5} }},")
-        return "\n".join(lines)
+        return "\n".join(lines) + "\n"
 
     def split(self, rom_bytes):
         self.out_path().parent.mkdir(parents=True, exist_ok=True)

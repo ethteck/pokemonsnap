391702257f96adeb704ae27da9165577b37652a4
Ethan <ethteck@gmail.com>
2024-08-25 23:06:25 +0900

    wip

--- tools/splat_ext/snap_height_map_patches.py
+++ tools/splat_ext/snap_height_map_patches.py
@@ -1,9 +1,9 @@
-from splat.segtypes.n64.segment import N64Segment
+from splat.segtypes.segment import Segment
 from splat.util import options
 from pathlib import Path
 import struct
 
-class N64SegSnap_height_map_patches(N64Segment):
+class N64SegSnap_height_map_patches(Segment):
     def scan(self, rom_bytes):
         data = rom_bytes[self.rom_start : self.rom_end]
         self.file_text = self.disassemble_data(data)
@@ -17,7 +17,7 @@ class N64SegSnap_height_map_patches(N64Segment):
             assert (surface & 0xFF) == 0
             surface_enum_name = f"SURFACE_TYPE_{(surface >> 8):06X}"
             lines.append(f"{{ {A:12.6f}, {B:12.6f}, {C:12.6f}, {D:12.6f}, {surface_enum_name} << 8 }},")
-        return "\n".join(lines)
+        return "\n".join(lines) + "\n"
 
     def split(self, rom_bytes):
         self.out_path().parent.mkdir(parents=True, exist_ok=True)

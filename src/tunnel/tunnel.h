0df9fc8d59d9534acae56696bc6ab21226492f66
Unnunu <serge_zorkin@mail.ru>
2024-10-31 13:54:56 +0300

    main code

---
+++ src/tunnel/tunnel.h
@@ -0,0 +1,8 @@
+#include "common.h"
+#include "ld_addrs.h"
+#include "../world/world.h"
+#include "app_level/app_level.h"
+#include "app_render/app_render.h"
+
+void func_802E2C70_5DFD40(s32, f32);
+void func_802ED5C8_5EA698(void);

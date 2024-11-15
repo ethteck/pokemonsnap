f33c1694056772c6834b1410891f4cbfd73247ab
Unnunu <serge_zorkin@mail.ru>
2024-11-15 14:14:09 +0300

    documented dugtrio

--- src/tunnel/dugtrio/code.c
+++ src/tunnel/dugtrio/code.c
@@ -6,18 +6,18 @@
 //extern AnimCmd* diglett_modelanim_rise[];
 //extern AnimCmd* diglett_modelanim_burrow[];
 
-void func_802E6574_5E3644(GObj*);
-void func_802E67B8_5E3888(GObj*);
-void func_802E6A30_5E3B00(GObj*);
-void func_802E6490_5E3560(GObj*);
-void func_802E654C_5E361C(GObj*);
-void func_802E65B4_5E3684(GObj*);
-void func_802E67F8_5E38C8(GObj*);
-void func_802E6A70_5E3B40(GObj*);
+void dugtrio_FirstIdle(GObj*);
+void dugtrio_SecondIdle(GObj*);
+void dugtrio_ThirdIdle(GObj*);
+void dugtrio_InitialState(GObj*);
+void dugtrio_PhotoTaken(GObj*);
+void dugtrio_FirstRise(GObj*);
+void dugtrio_SecondRise(GObj*);
+void dugtrio_ThirdRise(GObj*);
 
 s32 dugtrio_animsounds_rise[] = { SOUND_ID_219 };
 
-AnimationHeader D_802EEAC4_5EBB94 = {
+AnimationHeader dugtrio_animation_look_around = {
     0.65,
     40,
     0x80165250,
@@ -25,7 +25,7 @@ AnimationHeader D_802EEAC4_5EBB94 = {
     NULL
 };
 
-AnimationHeader D_802EEAD8_5EBBA8 = {
+AnimationHeader dugtrio_animation_turn = {
     0.7,
     70,
     0x80165F90,
@@ -33,7 +33,7 @@ AnimationHeader D_802EEAD8_5EBBA8 = {
     NULL
 };
 
-AnimationHeader D_802EEAEC_5EBBBC = {
+AnimationHeader dugtrio_animation_unburrow = {
     0.7,
     35,
     0x80167220,
@@ -41,7 +41,7 @@ AnimationHeader D_802EEAEC_5EBBBC = {
     NULL
 };
 
-AnimationHeader D_802EEB00_5EBBD0 = {
+AnimationHeader dugtrio_animation_rise = {
     0.7,
     25,
     0x80166B50,
@@ -49,7 +49,7 @@ AnimationHeader D_802EEB00_5EBBD0 = {
     dugtrio_animsounds_rise
 };
 
-AnimationHeader D_802EEB14_5EBBE4 = {
+AnimationHeader dugtrio_animation_burrow = {
     0.7,
     35,
     0x80165B30,
@@ -57,36 +57,36 @@ AnimationHeader D_802EEB14_5EBBE4 = {
     NULL
 };
 
-InteractionHandler D_802EEB28_5EBBF8[] = {
-    { POKEMON_CMD_24, NULL, 0, func_802E654C_5E361C },
+InteractionHandler dugtrio_tg_ReadyForPhoto[] = {
+    { POKEMON_CMD_24, NULL, 0, dugtrio_PhotoTaken },
     { POKEMON_CMD_58, NULL, 0, NULL },
 };
 
-InteractionHandler D_802EEB48_5EBC18[] = {
-    { TUNNEL_CMD_34, func_802E65B4_5E3684, 0, NULL },
+InteractionHandler dugtrio_tg_First[] = {
+    { TUNNEL_CMD_34, dugtrio_FirstRise, 0, NULL },
     { POKEMON_CMD_58, NULL, 0, NULL },
 };
 
-InteractionHandler D_802EEB68_5EBC38[] = {
-    { TUNNEL_CMD_35, func_802E67F8_5E38C8, 0, NULL },
+InteractionHandler dugtrio_tg_Second[] = {
+    { TUNNEL_CMD_35, dugtrio_SecondRise, 0, NULL },
     { POKEMON_CMD_58, NULL, 0, NULL },
 };
 
-InteractionHandler D_802EEB88_5EBC58[] = {
-    { TUNNEL_CMD_36, func_802E6A70_5E3B40, 0, NULL },
+InteractionHandler dugtrio_tg_Third[] = {
+    { TUNNEL_CMD_36, dugtrio_ThirdRise, 0, NULL },
     { POKEMON_CMD_58, NULL, 0, NULL },
 };
 
 PokemonAnimationSetup dugtrio_animSetup = {
-    &D_802EEAC4_5EBB94,
-    func_802E6490_5E3560,
+    &dugtrio_animation_look_around,
+    dugtrio_InitialState,
     0,
     { 0, 0, 0 },
     NULL,
     NULL
 };
 
-PokemonInitData D_802EEBBC_5EBC8C = {
+PokemonInitData dugtrio_initData = {
     0x80330670,
     NULL,
     renderPokemonModelTypeBFogged,
@@ -101,42 +101,42 @@ PokemonInitData D_802EEBBC_5EBC8C = {
     { 0, 0, 0 }
 };
 
-extern s32 D_80343148_640218;
-extern s32 D_8034314C_64021C;
+extern s32 dugtrio_IsPhotoTaken;
+extern s32 dugtrio_MissedPhotoCount;
 
-POKEMON_FUNC(func_802E6490_5E3560)
+POKEMON_FUNC(dugtrio_InitialState)
     pokemon->tangible = false;
     pokemon->flags |= POKEMON_FLAG_8;
     pokemon->miscVars->field0 = pokemon->collisionRadius;
     obj->flags |= GOBJ_FLAG_HIDDEN | GOBJ_FLAG_2;
 
     if (pokemon->behavior == 1) {
-        Pokemon_SetState(obj, func_802E6574_5E3644);
+        Pokemon_SetState(obj, dugtrio_FirstIdle);
     }
     if (pokemon->behavior == 2) {
-        Pokemon_SetState(obj, func_802E67B8_5E3888);
+        Pokemon_SetState(obj, dugtrio_SecondIdle);
     }
     if (pokemon->behavior == 3) {
-        Pokemon_SetState(obj, func_802E6A30_5E3B00);
+        Pokemon_SetState(obj, dugtrio_ThirdIdle);
     }
 
     Pokemon_SetState(obj, NULL);
 }
 
-POKEMON_FUNC(func_802E654C_5E361C)
-    D_80343148_640218 = true;
+POKEMON_FUNC(dugtrio_PhotoTaken)
+    dugtrio_IsPhotoTaken = true;
     Pokemon_StopAuxProc(obj);
 }
 
-POKEMON_FUNC(func_802E6574_5E3644)
-    pokemon->transitionGraph = D_802EEB48_5EBC18;
+POKEMON_FUNC(dugtrio_FirstIdle)
+    pokemon->transitionGraph = dugtrio_tg_First;
     Pokemon_WaitForFlag(obj, 0);
     Pokemon_SetState(obj, NULL);
 }
 
-POKEMON_FUNC(func_802E65B4_5E3684)
+POKEMON_FUNC(dugtrio_FirstRise)
     D_802EEEC8_5EBF98 = obj;
-    D_80343148_640218 = false;
+    dugtrio_IsPhotoTaken = false;
 
     pokemon->counter = 60, pokemon->processFlags &= ~POKEMON_PROCESS_WAIT_ENDED;
     pokemon->transitionGraph = NULL;
@@ -145,7 +145,7 @@ POKEMON_FUNC(func_802E65B4_5E3684)
     pokemon->tangible = true;
     obj->flags = 0;
     pokemon->collisionRadius = 0.0f;
-    Pokemon_SetAnimation(obj, &D_802EEAEC_5EBBBC);
+    Pokemon_SetAnimation(obj, &dugtrio_animation_unburrow);
 
     pokemon->counter = 48, pokemon->processFlags &= ~POKEMON_PROCESS_WAIT_ENDED;
     pokemon->transitionGraph = NULL;
@@ -157,47 +157,47 @@ POKEMON_FUNC(func_802E65B4_5E3684)
     Pokemon_WaitForFlag(obj, POKEMON_PROCESS_FLAG_ANIMATION_ENDED);
 
     pokemon->flags &= ~POKEMON_FLAG_8;
-    Pokemon_SetAnimation(obj, &D_802EEB00_5EBBD0);
-    pokemon->transitionGraph = D_802EEB28_5EBBF8;
+    Pokemon_SetAnimation(obj, &dugtrio_animation_rise);
+    pokemon->transitionGraph = dugtrio_tg_ReadyForPhoto;
     Pokemon_WaitForFlag(obj, POKEMON_PROCESS_FLAG_ANIMATION_ENDED);
 
-    Pokemon_SetAnimation(obj, &D_802EEAC4_5EBB94);
-    pokemon->counter = 30; pokemon->processFlags &= ~POKEMON_PROCESS_WAIT_ENDED, pokemon->transitionGraph = D_802EEB28_5EBBF8;
+    Pokemon_SetAnimation(obj, &dugtrio_animation_look_around);
+    pokemon->counter = 30; pokemon->processFlags &= ~POKEMON_PROCESS_WAIT_ENDED, pokemon->transitionGraph = dugtrio_tg_ReadyForPhoto;
     Pokemon_WaitForFlag(obj, POKEMON_PROCESS_WAIT_ENDED);
 
-    Pokemon_SetAnimation(obj, &D_802EEAD8_5EBBA8);
-    pokemon->transitionGraph = D_802EEB28_5EBBF8;
+    Pokemon_SetAnimation(obj, &dugtrio_animation_turn);
+    pokemon->transitionGraph = dugtrio_tg_ReadyForPhoto;
     Pokemon_WaitForFlag(obj, POKEMON_PROCESS_FLAG_ANIMATION_ENDED);
 
     pokemon->flags |= POKEMON_FLAG_8;
     pokemon->collisionRadius = 0.0f;
-    Pokemon_SetAnimation(obj, &D_802EEB14_5EBBE4);
+    Pokemon_SetAnimation(obj, &dugtrio_animation_burrow);
     pokemon->transitionGraph = NULL;
     Pokemon_WaitForFlag(obj, POKEMON_PROCESS_FLAG_ANIMATION_ENDED);
 
     pokemon->tangible = false;
     obj->flags |= GOBJ_FLAG_HIDDEN | GOBJ_FLAG_2;
 
-    if (D_80343148_640218) {
+    if (dugtrio_IsPhotoTaken) {
         D_802EEED0_5EBFA0 = 8;
         cmdSendCommandToLink(LINK_POKEMON, TUNNEL_CMD_35, obj);
     } else {
         cmdSendCommandToLink(LINK_POKEMON, TUNNEL_CMD_FIRST_DIGLETT_SHOW_UP, obj);
     }
 
-    Pokemon_SetState(obj, func_802E6574_5E3644);
+    Pokemon_SetState(obj, dugtrio_FirstIdle);
 }
 
-POKEMON_FUNC(func_802E67B8_5E3888)
-    pokemon->transitionGraph = D_802EEB68_5EBC38;
+POKEMON_FUNC(dugtrio_SecondIdle)
+    pokemon->transitionGraph = dugtrio_tg_Second;
     Pokemon_WaitForFlag(obj, 0);
     Pokemon_SetState(obj, NULL);
 }
 
-POKEMON_FUNC(func_802E67F8_5E38C8)
-    D_8034314C_64021C = 0;
+POKEMON_FUNC(dugtrio_SecondRise)
+    dugtrio_MissedPhotoCount = 0;
     D_802EEEC8_5EBF98 = obj;
-    D_80343148_640218 = false;
+    dugtrio_IsPhotoTaken = false;
 
     pokemon->counter = 60, pokemon->processFlags &= ~POKEMON_PROCESS_WAIT_ENDED;
     pokemon->transitionGraph = NULL;
@@ -206,7 +206,7 @@ POKEMON_FUNC(func_802E67F8_5E38C8)
     pokemon->tangible = true;
     obj->flags = 0;
     pokemon->collisionRadius = 0.0f;
-    Pokemon_SetAnimation(obj, &D_802EEAEC_5EBBBC);
+    Pokemon_SetAnimation(obj, &dugtrio_animation_unburrow);
 
     pokemon->counter = 48, pokemon->processFlags &= ~POKEMON_PROCESS_WAIT_ENDED;
     pokemon->transitionGraph = NULL;
@@ -218,48 +218,48 @@ POKEMON_FUNC(func_802E67F8_5E38C8)
     Pokemon_WaitForFlag(obj, POKEMON_PROCESS_FLAG_ANIMATION_ENDED);
 
     pokemon->flags &= ~POKEMON_FLAG_8;
-    Pokemon_SetAnimation(obj, &D_802EEB00_5EBBD0);
-    pokemon->transitionGraph = D_802EEB28_5EBBF8;
+    Pokemon_SetAnimation(obj, &dugtrio_animation_rise);
+    pokemon->transitionGraph = dugtrio_tg_ReadyForPhoto;
     Pokemon_WaitForFlag(obj, POKEMON_PROCESS_FLAG_ANIMATION_ENDED);
 
-    Pokemon_SetAnimation(obj, &D_802EEAC4_5EBB94);
-    pokemon->counter = 30; pokemon->processFlags &= ~POKEMON_PROCESS_WAIT_ENDED, pokemon->transitionGraph = D_802EEB28_5EBBF8;
+    Pokemon_SetAnimation(obj, &dugtrio_animation_look_around);
+    pokemon->counter = 30; pokemon->processFlags &= ~POKEMON_PROCESS_WAIT_ENDED, pokemon->transitionGraph = dugtrio_tg_ReadyForPhoto;
     Pokemon_WaitForFlag(obj, POKEMON_PROCESS_WAIT_ENDED);
 
-    Pokemon_SetAnimation(obj, &D_802EEAD8_5EBBA8);
-    pokemon->transitionGraph = D_802EEB28_5EBBF8;
+    Pokemon_SetAnimation(obj, &dugtrio_animation_turn);
+    pokemon->transitionGraph = dugtrio_tg_ReadyForPhoto;
     Pokemon_WaitForFlag(obj, POKEMON_PROCESS_FLAG_ANIMATION_ENDED);
 
     pokemon->flags |= POKEMON_FLAG_8;
     pokemon->collisionRadius = 0.0f;
-    Pokemon_SetAnimation(obj, &D_802EEB14_5EBBE4);
+    Pokemon_SetAnimation(obj, &dugtrio_animation_burrow);
     pokemon->transitionGraph = NULL;
     Pokemon_WaitForFlag(obj, POKEMON_PROCESS_FLAG_ANIMATION_ENDED);
 
     pokemon->tangible = false;
     obj->flags |= GOBJ_FLAG_HIDDEN | GOBJ_FLAG_2;
 
-    if (D_80343148_640218) {
+    if (dugtrio_IsPhotoTaken) {
         D_802EEED0_5EBFA0 = 0x10;
-        D_802EEEC8_5EBF98 = 0;
+        D_802EEEC8_5EBF98 = NULL;
         cmdSendCommandToLink(LINK_POKEMON, TUNNEL_CMD_36, obj);
     } else {
-        if (D_8034314C_64021C != 0) {
+        if (dugtrio_MissedPhotoCount != 0) {
             cmdSendCommandToLink(LINK_POKEMON, TUNNEL_CMD_FIRST_DIGLETT_SHOW_UP, obj);
         }
-        D_8034314C_64021C++;
+        dugtrio_MissedPhotoCount++;
     }
 
-    Pokemon_SetState(obj, func_802E67B8_5E3888);
+    Pokemon_SetState(obj, dugtrio_SecondIdle);
 }
 
-POKEMON_FUNC(func_802E6A30_5E3B00)
-    pokemon->transitionGraph = D_802EEB88_5EBC58;
+POKEMON_FUNC(dugtrio_ThirdIdle)
+    pokemon->transitionGraph = dugtrio_tg_Third;
     Pokemon_WaitForFlag(obj, 0);
     Pokemon_SetState(obj, NULL);
 }
 
-POKEMON_FUNC(func_802E6A70_5E3B40)
+POKEMON_FUNC(dugtrio_ThirdRise)
     D_802EEEC8_5EBF98 = obj;
 
     pokemon->counter = 60, pokemon->processFlags &= ~POKEMON_PROCESS_WAIT_ENDED;
@@ -269,7 +269,7 @@ POKEMON_FUNC(func_802E6A70_5E3B40)
     pokemon->tangible = true;
     obj->flags = 0;
     pokemon->collisionRadius = 0.0f;
-    Pokemon_SetAnimation(obj, &D_802EEAEC_5EBBBC);
+    Pokemon_SetAnimation(obj, &dugtrio_animation_unburrow);
 
     pokemon->counter = 48, pokemon->processFlags &= ~POKEMON_PROCESS_WAIT_ENDED;
     pokemon->transitionGraph = NULL;
@@ -281,21 +281,21 @@ POKEMON_FUNC(func_802E6A70_5E3B40)
     Pokemon_WaitForFlag(obj, POKEMON_PROCESS_FLAG_ANIMATION_ENDED);
 
     pokemon->flags &= ~POKEMON_FLAG_8;
-    Pokemon_SetAnimation(obj, &D_802EEB00_5EBBD0);
-    pokemon->transitionGraph = D_802EEB28_5EBBF8;
+    Pokemon_SetAnimation(obj, &dugtrio_animation_rise);
+    pokemon->transitionGraph = dugtrio_tg_ReadyForPhoto;
     Pokemon_WaitForFlag(obj, POKEMON_PROCESS_FLAG_ANIMATION_ENDED);
 
-    Pokemon_SetAnimation(obj, &D_802EEAC4_5EBB94);
-    pokemon->counter = 30; pokemon->processFlags &= ~POKEMON_PROCESS_WAIT_ENDED, pokemon->transitionGraph = D_802EEB28_5EBBF8;
+    Pokemon_SetAnimation(obj, &dugtrio_animation_look_around);
+    pokemon->counter = 30; pokemon->processFlags &= ~POKEMON_PROCESS_WAIT_ENDED, pokemon->transitionGraph = dugtrio_tg_ReadyForPhoto;
     Pokemon_WaitForFlag(obj, POKEMON_PROCESS_WAIT_ENDED);
 
-    Pokemon_SetAnimation(obj, &D_802EEAD8_5EBBA8);
-    pokemon->transitionGraph = D_802EEB28_5EBBF8;
+    Pokemon_SetAnimation(obj, &dugtrio_animation_turn);
+    pokemon->transitionGraph = dugtrio_tg_ReadyForPhoto;
     Pokemon_WaitForFlag(obj, POKEMON_PROCESS_FLAG_ANIMATION_ENDED);
 
     pokemon->flags |= POKEMON_FLAG_8;
     pokemon->collisionRadius = 0.0f;
-    Pokemon_SetAnimation(obj, &D_802EEB14_5EBBE4);
+    Pokemon_SetAnimation(obj, &dugtrio_animation_burrow);
     pokemon->transitionGraph = NULL;
     Pokemon_WaitForFlag(obj, POKEMON_PROCESS_FLAG_ANIMATION_ENDED);
 
@@ -306,7 +306,7 @@ POKEMON_FUNC(func_802E6A70_5E3B40)
 }
 
 GObj* dugtrio_Spawn(s32 objID, u16 id, WorldBlock* block, WorldBlock* blockB, ObjectSpawn* spawn) {
-    GObj* obj = Pokemon_SpawnOnGround(objID, id, block, blockB, spawn, &D_802EEBBC_5EBC8C);
+    GObj* obj = Pokemon_SpawnOnGround(objID, id, block, blockB, spawn, &dugtrio_initData);
     GET_POKEMON(obj)->tangible = false;
     obj->flags |= GOBJ_FLAG_HIDDEN | GOBJ_FLAG_2;
 }

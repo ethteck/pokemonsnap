8e22e8439604a90d9dc546f5e25bb514c3e85e3e
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2025-10-07 12:50:04 +0200

    Inline strings to fix macOS build (#138)

--- src/window/848B80.c
+++ src/window/848B80.c
@@ -1,13 +1,13 @@
 #include "common.h"
 
-extern UNK_TYPE D_803A66D0_879E80;
-extern UNK_TYPE D_803A6718_879EC8;
+char D_803A66D0_879E80[] = "どの  しゃしんを  アルバムに\nのこしますか\n〜あと  %sまい  のこせます〜";
+char D_803A6718_879EC8[] = "どの  しゃしんを  アルバムに\nのこしますか\n〜これいじょう  のこせません〜";
 
-UNK_TYPE* func_803753D0_848B80(s32 arg0) {
+char* func_803753D0_848B80(s32 arg0) {
     switch (arg0) {
         case 0:
-            return &D_803A66D0_879E80;
+            return D_803A66D0_879E80;
         case 1:
-            return &D_803A6718_879EC8;
+            return D_803A6718_879EC8;
     }
 }

bd05556e1b9a47e11b73d29a61b2f085f31b032c
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2025-10-17 11:30:16 +0200

    jynx (#140)

---
+++ src/cave/jynx/textures.c
@@ -0,0 +1,76 @@
+#include <common.h>
+
+__ALIGNER
+u8 jynx_tex_arm_png[] = {
+#include "assets/cave/jynx/tex_arm.png.bin.c"
+};
+
+__ALIGNER
+u8 jynx_tex_torso_pal[] = {
+#include "assets/cave/jynx/tex_torso.pal.bin.c"
+};
+
+__ALIGNER
+u8 jynx_tex_torso_png[] = {
+#include "assets/cave/jynx/tex_torso.png.bin.c"
+};
+
+__ALIGNER
+u8 jynx_tex_eye_0_pal[] = {
+#include "assets/cave/jynx/tex_eye_0.pal.bin.c"
+};
+
+__ALIGNER
+u8 jynx_tex_eye_0_png[] = {
+#include "assets/cave/jynx/tex_eye_0.png.bin.c"
+};
+
+__ALIGNER
+u8 jynx_tex_eye_1_pal[] = {
+#include "assets/cave/jynx/tex_eye_1.pal.bin.c"
+};
+
+__ALIGNER
+u8 jynx_tex_eye_1_png[] = {
+#include "assets/cave/jynx/tex_eye_1.png.bin.c"
+};
+
+__ALIGNER
+u8 jynx_tex_eye_2_pal[] = {
+#include "assets/cave/jynx/tex_eye_2.pal.bin.c"
+};
+
+__ALIGNER
+u8 jynx_tex_eye_2_png[] = {
+#include "assets/cave/jynx/tex_eye_2.png.bin.c"
+};
+
+__ALIGNER
+u8 jynx_tex_hair_png[] = {
+#include "assets/cave/jynx/tex_hair.png.bin.c"
+};
+
+__ALIGNER
+u8 jynx_tex_hair_2_png[] = {
+#include "assets/cave/jynx/tex_hair_2.png.bin.c"
+};
+
+__ALIGNER
+u8 jynx_tex_hair_3_png[] = {
+#include "assets/cave/jynx/tex_hair_3.png.bin.c"
+};
+
+__ALIGNER
+u8 jynx_tex_lips_png[] = {
+#include "assets/cave/jynx/tex_lips.png.bin.c"
+};
+
+__ALIGNER
+u8 jynx_tex_dress_pal[] = {
+#include "assets/cave/jynx/tex_dress.pal.bin.c"
+};
+
+__ALIGNER
+u8 jynx_tex_dress_png[] = {
+#include "assets/cave/jynx/tex_dress.png.bin.c"
+};

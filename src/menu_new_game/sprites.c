6b84480ce8f7b7acee309e698857583b659238a5
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2026-04-16 11:54:02 +0200

    menu_new_game

---
+++ src/menu_new_game/sprites.c
@@ -0,0 +1,10 @@
+#include <common.h>
+#include <PR/sp.h>
+
+#include "build/assets/menu_new_game/D_80109880_A84C30.png.inc.h"
+
+static u8 aligner_0[] = { 0, 0, 0, 0, 0, 0, 0, 0};
+
+#include "build/assets/menu_new_game/D_80117F98_A93348.png.inc.h"
+
+#include "build/assets/menu_new_game/D_80118068_A93418.png.inc.h"
